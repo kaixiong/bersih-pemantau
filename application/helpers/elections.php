2c0fe71474efd92cf7221d5bdd39696cae1c6edd
Chong Kai Xiong <kaixiong@codeleft.sg>
2013-05-14 06:10:03 +0800

    Make code formatting consistent with Ushahidi

--- application/helpers/elections.php
+++ application/helpers/elections.php
@@ -1,21 +1,29 @@
 <?php
 
 class elections_Core {
-	public static function parliament_seats() {
+	public static function parliament_seats()
+	{
 		$db = new Database();
+
 		$result = $db->query('SELECT * FROM `parliament_seats`');
-		foreach ($result as $row) {
+		foreach ($result as $row)
+		{
 			$seats[$row->id] = $row;
 		}
+
 		return $seats;
 	}
 
-	public static function state_seats($parliament_seat_id) {
+	public static function state_seats($parliament_seat_id)
+	{
 		$db = new Database();
+
 		$result = $db->query('SELECT `id`, `name`, `latitude`, `longitude` FROM `state_seats` WHERE `parliament_id` = ?', $parliament_seat_id);
-		foreach ($result as $row) {
+		foreach ($result as $row)
+		{
 			$seats[$row->id] = $row;
 		}
+
 		return $seats;
 	}
 }
