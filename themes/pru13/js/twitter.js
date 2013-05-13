d02709332c66f660b360e8c455591d5991278add
Chong Kai Xiong <kaixiong@codeleft.sg>
2013-05-13 13:44:16 +0800

    Clean up Twitter feed script and use https for tweet search

--- themes/pru13/js/twitter.js
+++ themes/pru13/js/twitter.js
@@ -1,57 +1,44 @@
 // by blacklizard for pru13.info
 $(document).ready(function() {
-	$.getJSON('http://search.twitter.com/search.json?callback=?&rpp=5&result_type=recent&q=jompantau' ,function(response){
-		twitterParse(response);
+	function formatDate(date) {
+		var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+		var dateStr = months[date.getMonth()] + ' ' + date.getDate();
+
+		var timeStr = '';
+		var minutesStr = '';
+
+		if (date.getMinutes() < 10) {
+			minutesStr = '0' + date.getMinutes();
+		} else {
+			minutesStr = date.getMinutes().toString();
+		}
+
+		if (date.getHours() <= 12) {
+			timeStr = date.getHours() + ':' + minutesStr + 'am';
+		} else {
+			timeStr = (date.getHours() - 12) + ':' + minutesStr + 'pm';
+		}
+
+		return dateStr + ', ' + timeStr;
+	}
+
+	$.getJSON('https://search.twitter.com/search.json?callback=?&rpp=5&result_type=recent&q=jompantau', function(response) {
+		// FIXME: Display error message if no tweets could be loaded
+
+		var feed = $('#twitter-feed');
+
+		$.each(response.results, function(index, entry) {
+			var html = '<div class="byline">';
+
+			html += '<span class="by"><a target="_blank" href="https://twitter.com/' + entry.from_user + '">';
+			html += entry.from_user;
+			html += '</a></span> '; // NOTE: Trailing space needed for spacing
+			html += '<span class="date">' + formatDate(new Date(entry.created_at)) + '</span></div>';
+			html += '<div class="tweet">' + entry.text + '</div>';
+			html += '</div>';
+
+			feed.append($('<div class="result"/>').html(html));
+		});
 	});
 });
-
-function twitterParse(response){
-
-	var tweetCount = response.results.length;
-	//to be used later to convert parsed data into jquery object
-	this.el = $('<div />');
-	var html = [];// container to store parsed data
-	var months = ['Jan', 'Feb', 'March', 'April', 'May', 'June','July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-	//i could use .each but what the heck.. lol
-	for (var i = 0; i < tweetCount; i++) {
-		html.push('<div class="result"><div class="byline"><span class="by"><a target="_blank" href="https://twitter.com/');
-		html.push(response.results[i].from_user);
-		html.push('">');
-		html.push(response.results[i].from_user);
-		html.push('</a></span><span class="date"> ');
-		
-		var date = new Date(response.results[i].created_at);
-		
-		var hour = date.getHours();
-		html.push(months[date.getMonth()]);
-		html.push(date.getDate());
-		html.push(', ');
-		html.push(hour);
-		html.push(':');
-		
-		var minutes = date.getMinutes();
-		
-		if(minutes<10){
-			minutes = '0'+minutes;
-		}
-		
-		html.push(minutes);
-		
-		if(hour>11){
-			html.push('pm');
-		}else{
-			html.push('am');
-		}
-		
-		html.push('</span></div><div class="tweet">');
-		html.push(response.results[i].text);
-		html.push('</div></div>');
-	}
-	//convert to jquery object
-	this.el.html(html.join(''));
-	
-	//get container
-	var $feed = $('#twitter-feed');
-	//append
-	$feed.append(this.el);
-}
